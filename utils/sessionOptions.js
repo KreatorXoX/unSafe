f5c05496235ba2b91473e17211a2fabac6088c47
Gorkem Gocer <gorkemgocer7@gmail.com>
2022-10-18 06:00:57 +0300

    sessions now stored in mongodb and ready to deploy

--- utils/sessionOptions.js
+++ utils/sessionOptions.js
@@ -1,5 +1,17 @@
+const MongoStore = require("connect-mongo");
+
+const store = MongoStore.create({
+  mongoUrl: `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.7xrkj.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`,
+  secret: process.env.SESSION_SECRET || "secretForSession",
+  touchAfter: 24 * 60 * 60,
+});
+
+store.on("error", function (err) {
+  console.log(err);
+});
 module.exports.sessionOptions = {
   name: "myAppSession",
+  store,
   secret: process.env.SESSION_SECRET || "secretForSession",
   resave: false,
   saveUninitialized: true,
